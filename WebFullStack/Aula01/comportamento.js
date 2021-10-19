e430ce763cb5044d192e9a722c6a0483048ad95a
pcaramguedes <pcaramguedes@gmail.com>
2021-10-18 22:25:59 -0300

    2 aula de FullStack

---
+++ WebFullStack/Aula01/comportamento.js
@@ -0,0 +1,21 @@
+/*
+
+Armazenamento de dados
+  let Chave = valor;
+
+*/
+
+let nome = "Paulo Caram";
+let idade = 52;
+let altura = 1.82;
+
+/*
+
+instruções (tarefas / Comandos)
+
+comando();
+
+
+*/
+alert("Boa noite !");
+alert(nome);
